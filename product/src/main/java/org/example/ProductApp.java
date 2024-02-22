b6499c4ab7bb91a0a40dc514b82496ebc8e74074
Nurisa <nurisa.abdykalykova03@gmail.com>
2024-02-22 11:02:10 +0600

    change logic

--- product/src/main/java/org/example/ProductApp.java
+++ product/src/main/java/org/example/ProductApp.java
@@ -1,27 +1,8 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ProductApp {
     public static void main( String[] args ) {
 
 
-    }
-
-    public static boolean isPalindrome(int x) {
-        if (x<0)
-            return false;
-        int copyNumber = 0;
-        int counter = x;
-        while(counter != 0){
-            copyNumber = copyNumber * 10 + counter%10;
-            counter = counter /10;
-        }
-        return copyNumber  == x;
-    }
-
-    public static int[] bubbleSort(int[] massive){
-        return new int[7];
     }
 }
